e689871f18bd6b63877e09eae7c0ee9061cf9d9b
Md Moshiur Rahman <moshi@MacBook-Pro.local>
2021-11-11 08:45:52 -0800

    Added new file from previous work

---
+++ moshi/java-code/Hello.java
@@ -0,0 +1,9 @@
+import org.apache.commons.lang3.StringUtils;
+public class Hello {
+    public static void main(String[] args) {
+	if (StringUtils.isNotBlank("a")) {
+	 System.out.println("Hello World!");
+	} 
+	
+    }
+}
