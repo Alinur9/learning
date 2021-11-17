0eccb27eebe08de8d539a6f6389fb69778698376
Ali Nur <novemberrainali@gmail.com>
2021-11-17 21:02:13 +0600

    Upadated Directory & File comparison class

---
+++ Alinur/File&Directory/DirectoryCompare.2.0/src/test/java/DirectoriesTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DirectoriesTest {
+
+    @Test
+    void compare() {
+        Directories.put("/home/alinur/IdeaProjects/Junit_Mockito/Test Folder/4",
+                "/home/alinur/IdeaProjects/Junit_Mockito/Test Folder/5");
+
+    }
+}
