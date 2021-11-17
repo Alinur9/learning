e2f0b2a45ea2f2a921c0c0db1421a6e7c18ec3b0
Ali Nur <novemberrainali@gmail.com>
2021-11-18 01:02:29 +0600

    Argument Captor

--- Alinur/File&Directory/DirectoryCompare.2.0/src/test/java/DirectoriesTest.java
+++ Alinur/File&Directory/DirectoryCompare.2.0/src/test/java/DirectoriesTest.java
@@ -1,13 +1,26 @@
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mockito;
+
+import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 class DirectoriesTest {
 
     @Test
-    void compare() {
+    void compare() throws IOException {
         Directories.put("/home/alinur/IdeaProjects/Junit_Mockito/Test Folder/4",
                 "/home/alinur/IdeaProjects/Junit_Mockito/Test Folder/5");
+        Log log = Mockito.mock(Log.class);
+     Directories.compare(log);
+        ArgumentCaptor<String> captor = ArgumentCaptor.forClass(String.class);
+        verify(log, times(1)).print(captor.capture());
 
+// Assert the argument
+        String actual = captor.getValue();
+        assertEquals("The Directories are same!", actual);
     }
 }
