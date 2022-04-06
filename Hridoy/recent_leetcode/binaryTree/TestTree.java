135d5ed2883c0e14a591da1472bff55a3cf88dbc
Ahsan <gamingcaptain10@gmail.com>
2022-04-06 14:07:51 +0600

    inOrder preOrder postOrder

---
+++ Hridoy/recent_leetcode/binaryTree/TestTree.java
@@ -0,0 +1,24 @@
+package com.binaryTree;
+
+public class TestTree {
+    public static void main(String[] args) {
+    TreeNode tree = new TreeNode();
+
+  tree.insert(5);
+  tree.insert(3);
+  tree.insert(4);
+  tree.insert(2);
+  tree.insert(6);
+
+
+
+
+
+      tree.printInOrder();
+
+
+
+
+
+}
+}
