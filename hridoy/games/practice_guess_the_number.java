7516c0320e00d2162c282505f0b2cc7d5c2ba03d
Ahsan <gamingcaptain10@gmail.com>
2022-04-07 11:25:45 +0600

    guess num file2

---
+++ hridoy/games/practice_guess_the_number.java
@@ -0,0 +1,7 @@
+
+public class practice_guess_the_number {
+    public static void main(String[] args) {
+        Practice_Guess_game game = new Practice_Guess_game();
+        game.playTheGame();
+    }
+}
