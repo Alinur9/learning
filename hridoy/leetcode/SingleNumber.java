19cabafeecbb76e0410703f757a5e94fb2468448
Ahsan <gamingcaptain10@gmail.com>
2022-06-06 13:55:48 +0600

    contest

---
+++ hridoy/leetcode/SingleNumber.java
@@ -0,0 +1,49 @@
+package leetcode.com.company;
+
+import java.util.Arrays;
+
+public class SingleNumber {
+    public int singleNumber(int[] nums) {
+//        int ans =0;
+//        int count ;
+//       for (int i =0; i <nums.length;i++){
+//           count =0;
+//         for (int j =0 ; j < nums.length;j++){
+//             if (nums[i] == nums[j]){
+//                 count++;
+//             }
+//
+//           }
+//         if (count == 1){
+//           ans = nums[i];
+//         }
+//        }
+//        return ans;
+//
+        int ans =0;
+
+        String s = (Arrays.toString(nums));
+        for (char c : s.toCharArray()){
+
+            int fidx = s.indexOf(c);
+            int lidx =s.lastIndexOf(c);
+
+            if (fidx == lidx){
+                 ans = Integer.parseInt(String.valueOf(c));
+                return ans;
+            }
+
+        }
+        return ans;
+
+    }
+
+    public static void main(String[] args) {
+        int [] nums =  {2,2,1};
+        SingleNumber obj = new SingleNumber();
+        System.out.println(obj.singleNumber(nums));
+
+    }
+
+
+}
