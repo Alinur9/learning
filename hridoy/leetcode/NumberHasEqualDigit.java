db6a873c6d166b099003bfafd277aedd57583205
Ahsan <gamingcaptain10@gmail.com>
2022-06-06 13:46:43 +0600

    contest

---
+++ hridoy/leetcode/NumberHasEqualDigit.java
@@ -0,0 +1,26 @@
+package leetcode.com.company;
+
+public class NumberHasEqualDigit {
+    public boolean digitCount(String num) {
+        int count = 0;
+        char[] ch = num.toCharArray();
+        for (int i =0 ; i < ch.length;i++){
+            for ( int j = 0 ; j < ch.length; j++){
+                if (i == Character.getNumericValue(ch[j])){
+                    count ++;
+                }
+            }
+            int occur = Character.getNumericValue(ch[i]);
+            if (count != occur){
+                return  false;
+            }
+            count = 0;
+        }
+    return true;
+    }
+    public static void main(String[] args) {
+        String num = "030";
+        NumberHasEqualDigit obj = new NumberHasEqualDigit();
+        System.out.println(obj.digitCount(num));
+    }
+}
