95d643237f4fc6738dc9e3eba540e951c3d44472
Ahsan <gamingcaptain10@gmail.com>
2022-06-06 12:39:21 +0600

    Contest

---
+++ hridoy/leetcode/BinaryCodesOfSizeK.java
@@ -0,0 +1,30 @@
+package leetcode.com.company;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class BinaryCodesOfSizeK {
+
+    public boolean hasAllCodes(String s, int k) {
+
+        Set<String> set = new HashSet<>();
+           int i =0;
+            while ( i <= s.length()-k ){
+                String sub = s.substring(i, i+k);
+                set.add(sub);
+                if (set.size() == Math.pow(2,k)){
+                    return true;
+            }
+                i++;
+        }
+        return false;
+
+    }
+    public static void main(String[] args) {
+        String s = "00110110";
+        int k =2;
+
+        BinaryCodesOfSizeK obj = new BinaryCodesOfSizeK();
+        System.out.println(obj.hasAllCodes(s,k));
+    }
+}
