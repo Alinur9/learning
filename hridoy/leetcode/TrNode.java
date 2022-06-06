03bd68e7e01d9148531b1708deb002aea68e6aeb
Ahsan <gamingcaptain10@gmail.com>
2022-06-06 17:10:12 +0600

    contest

---
+++ hridoy/leetcode/TrNode.java
@@ -0,0 +1,19 @@
+package leetcode.com.company;
+
+
+    public  class TrNode {
+        int value;
+        TrNode left;
+        TrNode right;
+
+        TrNode() {
+        }
+
+        TrNode(int value) {
+            this.value = value;
+            left = null;
+            right = null;
+        }
+    }
+
+
