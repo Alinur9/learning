7961839fc299ba0959451dbc75edbf13ecbd5260
Ahsan <gamingcaptain10@gmail.com>
2022-06-06 14:00:17 +0600

    contest

---
+++ hridoy/leetcode/SuffleString.java
@@ -0,0 +1,27 @@
+package leetcode.com.company;
+
+public class SuffleString {
+    public String restoreString(String s, int[] indices) {
+
+        int n = s.length();
+        char[] ch = new char[s.length()];
+        for (int i =0 ; i < n ; i++){
+            for (int j =0 ; j< n ; j++){
+                if ( indices[j] == i){
+                    ch[i] = s.charAt(j);
+                    break;
+                }
+            }
+        }
+        String ans = String.valueOf(ch);
+        return ans;
+
+    }
+    public static void main(String[] args) {
+        String s = "codeleet";
+        int[] indices = {4,5,6,7,0,2,1,3};
+        SuffleString obj = new SuffleString();
+        System.out.println(obj.restoreString(s,indices));
+
+    }
+}
